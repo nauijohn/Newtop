8c490a265011203f2195103093278df54002bdde
nauijohn <85679013+nauijohn@users.noreply.github.com>
2023-01-26 00:00:11 +0800

    enchanced contact

--- contact.php
+++ contact.php
@@ -1,20 +1,27 @@
 <?php
-$my_arr = array(1,2,3);
-echo '<script>';
-echo 'console.log('. json_encode($my_arr, JSON_HEX_TAG) .')';
-echo '</script>';
-if(!$_POST) {
+// $my_arr = array(1,2,3);
+// echo '<script>';
+// echo 'console.log('. json_encode($my_arr, JSON_HEX_TAG) .')';
+// echo '</script>';
 
-	exit};
+if(!$_POST) exit;
+
+
 
 $email = $_POST['email'];
 
+$errors=0;
+$error="";
 
 //$error[] = preg_match('/\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i', $_POST['email']) ? '' : 'INVALID EMAIL ADDRESS';
 // if(!eregi("^[a-z0-9]+([_\\.-][a-z0-9]+)*" ."@"."([a-z0-9]+([\.-][a-z0-9]+)*)+"."\\.[a-z]{2,}"."$",$email )){
-if(!eregi("^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$",$email )){
+if(!preg_match("/^[a-z0-9]+([_\\.-][a-z0-9]+)*" ."@"."([a-z0-9]+([\.-][a-z0-9]+)*)+"."\\.[a-z]{2,}"."$/",$email )){
+// if(!preg_match("/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/",trim($email) )){
 	$error.="Invalid email address entered";
 	$errors=1;
+	// echo '<script>';
+	// echo 'console.log('. json_encode($error, JSON_HEX_TAG) .')';
+	// echo '</script>';
 }
 if($errors==1) echo $error;
 else{
